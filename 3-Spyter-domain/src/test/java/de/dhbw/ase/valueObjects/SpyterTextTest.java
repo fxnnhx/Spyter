d44fef9fd1bb1163e0d57c1760483fc68f5d2f85
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-18 15:30:50 +0100

    broken getWords test

---
+++ 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterTextTest.java
@@ -0,0 +1,21 @@
+package de.dhbw.ase.valueObjects;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SpyterTextTest {
+
+
+    @Test
+    void getWords() {
+        String testString = "ab cd";
+        SpyterWord ab_word = List.of("ab ".toCharArray(), "ab".toCharArray()).stream()
+        List<SpyterWord> expectedWords = List.of(
+
+        );
+    }
+}
