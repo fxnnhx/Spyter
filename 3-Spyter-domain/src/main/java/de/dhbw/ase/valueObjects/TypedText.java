d8f93847c695bb2e3d9a3431bfcb2316ce67a6f7
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-18 13:22:23 +0100

    constructor for TypedText

--- 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/TypedText.java
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/TypedText.java
@@ -3,5 +3,9 @@ package de.dhbw.ase.valueObjects;
 import java.util.List;
 
 public class TypedText {
-    private List<TypedCharacter> typedCharacters;
+    private final List<TypedCharacter> typedCharacters;
+
+    public TypedText(List<TypedCharacter> typedCharacters) {
+        this.typedCharacters = typedCharacters;
+    }
 }
