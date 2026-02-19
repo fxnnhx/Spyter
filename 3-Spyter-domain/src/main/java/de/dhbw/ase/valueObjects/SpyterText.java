e785e13d28ecedc30b3c27df57d7cfa353269771
key.collector <key.collector@web.de>
2026-02-19 16:02:04 +0100

    implement SpyterText Constructor from native String

--- 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/SpyterText.java
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/SpyterText.java
@@ -10,6 +10,14 @@ public class SpyterText {
         this.characters = characters;
     }
 
+    public SpyterText(String text) {
+        List<SpyterCharacter> spyterCharacters = new ArrayList<>();
+        for (char c : text.toCharArray()) {
+            SpyterCharacter.tryFrom(c).ifPresent(spyterCharacters::add);
+        }
+        this.characters = spyterCharacters;
+    }
+
     public List<SpyterWord> getWords() {
         List<SpyterWord> words = new ArrayList<>();
         int beginOfWord = 0, endOfWord = 0;
