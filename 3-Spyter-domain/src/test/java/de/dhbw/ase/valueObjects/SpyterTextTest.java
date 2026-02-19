d60efe37eded8c6fa5aaa81bb574fd381ebebefd
key.collector <key.collector@web.de>
2026-02-19 15:48:38 +0100

    test SpyterText.getWords Method

--- 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterTextTest.java
+++ 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterTextTest.java
@@ -2,6 +2,7 @@ package de.dhbw.ase.valueObjects;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -12,13 +13,18 @@ class SpyterTextTest {
 
     @Test
     void getWords() {
-        String testString = "ab cd";
+        List<SpyterCharacter> testString = List.of(
+                SpyterCharacter.tryFrom('a').get(),
+                SpyterCharacter.tryFrom('b').get(),
+                SpyterCharacter.tryFrom(' ').get(),
+                SpyterCharacter.tryFrom('c').get(),
+                SpyterCharacter.tryFrom('d').get()
+        );
         List<SpyterWord> expectedWords = List.of(
-                new SpyterWord(List.of(new SpyterCharacter('a'), new SpyterCharacter('b'), new SpyterCharacter(' '))),
-                new SpyterWord(List.of(new SpyterCharacter('c'), new SpyterCharacter('d')))
+                new SpyterWord(List.of(testString.get(0), testString.get(1), testString.get(2))),
+                new SpyterWord(List.of(testString.get(3), testString.get(4)))
         );
         SpyterText text = new SpyterText(testString);
-
-        text.getWords().equals(expectedWords);
+        assertTrue(text.getWords().equals(expectedWords));
     }
 }
