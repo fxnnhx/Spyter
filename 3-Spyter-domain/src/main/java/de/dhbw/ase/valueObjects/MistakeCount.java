b877e5d29a8abb16db8c98848e87d169c9455bdb
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-24 14:52:25 +0100

    add MistakeCount definition

---
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/MistakeCount.java
@@ -0,0 +1,20 @@
+package de.dhbw.ase.valueObjects;
+
+public class MistakeCount {
+    private int count = 0;
+
+    public MistakeCount() {
+    }
+
+    public void addMistake() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void removeMistake() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public int getValue() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
