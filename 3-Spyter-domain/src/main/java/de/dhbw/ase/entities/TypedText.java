e4e5b29d95299fc7198aa9145b1754b3f35e29ba
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-24 15:50:50 +0100

    Initialize typedCharacters in constructor and implement push, pop, and length methods in TypedText

--- 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/TypedText.java
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/TypedText.java
@@ -1,20 +1,28 @@
 package de.dhbw.ase.entities;
 
 import de.dhbw.ase.valueObjects.TypedCharacter;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class TypedText {
     private final List<TypedCharacter> typedCharacters;
 
     public TypedText(int size) {
-        throw new UnsupportedOperationException("Not yet supported");
+        this.typedCharacters = new ArrayList<>(size);
     }
 
     public void push(TypedCharacter typedCharacter) {
-        throw new UnsupportedOperationException("Not yet supported");
+        this.typedCharacters.add(typedCharacter);
     }
 
     public void pop() {
-        throw new UnsupportedOperationException("Not yet supported");
+        if (!this.typedCharacters.isEmpty()) {
+            this.typedCharacters.removeLast();
+        }
+    }
+
+    public int length() {
+        return this.typedCharacters.size();
     }
 }
