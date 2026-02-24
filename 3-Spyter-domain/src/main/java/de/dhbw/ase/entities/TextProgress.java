b0b6746b5788330231ac37caf19d3013b928a4e4
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-24 14:48:54 +0100

    define TextProgress

---
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/TextProgress.java
@@ -0,0 +1,31 @@
+package de.dhbw.ase.entities;
+
+import de.dhbw.ase.valueObjects.CharacterCorrectionType;
+import de.dhbw.ase.valueObjects.SpyterCharacter;
+import de.dhbw.ase.valueObjects.SpyterText;
+
+public class TextProgress {
+    private final SpyterText text;
+    private final TypedText typedText;
+
+    public TextProgress(SpyterText text) {
+       this.text = text;
+       this.typedText = new TypedText(text.length());
+    }
+
+    public void advance(CharacterCorrectionType correctionType) {
+       throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean isNextChar(SpyterCharacter character){
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public void remoteLastCharacter() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public boolean isFinished() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
