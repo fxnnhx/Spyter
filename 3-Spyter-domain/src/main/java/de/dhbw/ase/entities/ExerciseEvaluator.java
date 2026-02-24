289ce6f9ccd709d597b7b4988083b2cba6c438c2
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-24 14:56:55 +0100

    add ExerciseEvaluator definition

---
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/ExerciseEvaluator.java
@@ -0,0 +1,18 @@
+package de.dhbw.ase.entities;
+
+import de.dhbw.ase.valueObjects.ExerciseResult;
+import de.dhbw.ase.valueObjects.MistakeCount;
+import de.dhbw.ase.valueObjects.TimeDelta;
+
+public class ExerciseEvaluator {
+    private final MistakeCount mistakeCount;
+    private final TimeDelta timeDelta;
+
+    public ExerciseEvaluator(MistakeCount mistakeCount, TimeDelta timeDelta) {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    public ExerciseResult generateExerciseResult() {
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
