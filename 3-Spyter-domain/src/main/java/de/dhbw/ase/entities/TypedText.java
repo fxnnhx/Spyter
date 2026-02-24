a5f74cf9aa1ad624fde92695f0b3ce1fec2e70e0
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-02-24 14:49:13 +0100

    define and move TypedText

---
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/TypedText.java
@@ -0,0 +1,20 @@
+package de.dhbw.ase.entities;
+
+import de.dhbw.ase.valueObjects.TypedCharacter;
+import java.util.List;
+
+public class TypedText {
+    private final List<TypedCharacter> typedCharacters;
+
+    public TypedText(int size) {
+        throw new UnsupportedOperationException("Not yet supported");
+    }
+
+    public void push(TypedCharacter typedCharacter) {
+        throw new UnsupportedOperationException("Not yet supported");
+    }
+
+    public void pop() {
+        throw new UnsupportedOperationException("Not yet supported");
+    }
+}
