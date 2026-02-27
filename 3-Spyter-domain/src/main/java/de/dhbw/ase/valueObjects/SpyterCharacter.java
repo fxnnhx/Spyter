0e48ff1edad65d1b05d2f5b877814783882b1ef3
key.collector <key.collector@web.de>
2026-02-27 18:54:31 +0100

    [broken] use characterDomain in SpyterCharacter class

--- 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/SpyterCharacter.java
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/valueObjects/SpyterCharacter.java
@@ -5,18 +5,20 @@ import java.util.Optional;
 
 public class SpyterCharacter {
 
+  CharacterDomain domain;
   private final char value;
 
-  private SpyterCharacter(char value) {
+  private SpyterCharacter(CharacterDomain domain, char value) {
+    this.domain = domain;
     this.value = value;
   }
 
   public boolean isDelimiter() {
-    return CharacterDomain.isDelimiter(this.value);
+    return domain.isDelimiter(this.value);
   }
 
   public KeyStrokeCount getKeyStrokeCount(){
-    return CharacterDomain.keyStrokeOfCharacter(this);
+    return domain.keyStrokeOfCharacter(this);
   }
 
   @Override
@@ -26,9 +28,9 @@ public class SpyterCharacter {
     return this.value == other.value;
   }
 
-  public static Optional<SpyterCharacter> tryFrom(char value) {
-    if (CharacterDomain.isDomainCharacter(value)) {
-      return Optional.of(new SpyterCharacter(value));
+  public static Optional<SpyterCharacter> tryFrom(CharacterDomain domain, char value) {
+    if (domain.isDomainCharacter(value)) {
+      return Optional.of(new SpyterCharacter(domain, value));
     } else {
       return Optional.empty();
     }
