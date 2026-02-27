712372da1f3f984013e36be4b5ccf6c045b6d2b6
key.collector <key.collector@web.de>
2026-02-27 19:01:13 +0100

    [broken] use characterDomain in SpyterCharacter tests

--- 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterCharacterTest.java
+++ 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterCharacterTest.java
@@ -1,5 +1,6 @@
 package de.dhbw.ase.valueObjects;
 
+import de.dhbw.ase.constants.CharacterDomain;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -9,7 +10,30 @@ class SpyterCharacterTest {
     @Test
     void getValue() {
         char c = 'a';
-        SpyterCharacter spyterCharacter = SpyterCharacter.tryFrom(c).get();
-        assertEquals(c, spyterCharacter.getValue());
+        SpyterCharacter spyterCharacter = SpyterCharacter.tryFrom(new MockCharacterDomain(c), c).get();
+        assert(c == spyterCharacter.getValue());
+    }
+}
+
+class MockCharacterDomain implements CharacterDomain {
+    char character;
+
+    public MockCharacterDomain(char allowedCharacter) {
+        this.character = allowedCharacter;
+    }
+
+    @Override
+    public boolean isDelimiter(char character) {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+
+    @Override
+    public boolean isDomainCharacter(char character) {
+        return character == this.character;
+    }
+
+    @Override
+    public KeyStrokeCount keyStrokeOfCharacter(SpyterCharacter character) {
+        throw new UnsupportedOperationException("Not implemented");
     }
 }
