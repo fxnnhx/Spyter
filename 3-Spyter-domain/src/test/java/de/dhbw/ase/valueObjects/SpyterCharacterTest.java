ccf9fc3ef567dff2cd116201d5260c4a13387759
key.collector <key.collector@web.de>
2026-02-27 19:13:17 +0100

    make mocked CharacterDomain internal class

--- 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterCharacterTest.java
+++ 3-Spyter-domain/src/test/java/de/dhbw/ase/valueObjects/SpyterCharacterTest.java
@@ -13,27 +13,27 @@ class SpyterCharacterTest {
         SpyterCharacter spyterCharacter = SpyterCharacter.tryFrom(new MockCharacterDomain(c), c).get();
         assert(c == spyterCharacter.getValue());
     }
-}
 
-class MockCharacterDomain implements CharacterDomain {
-    char character;
+    class MockCharacterDomain implements CharacterDomain {
+        char character;
 
-    public MockCharacterDomain(char allowedCharacter) {
-        this.character = allowedCharacter;
-    }
+        public MockCharacterDomain(char allowedCharacter) {
+            this.character = allowedCharacter;
+        }
 
-    @Override
-    public boolean isDelimiter(char character) {
-        throw new UnsupportedOperationException("Not implemented");
-    }
+        @Override
+        public boolean isDelimiter(char character) {
+            throw new UnsupportedOperationException("Not implemented");
+        }
 
-    @Override
-    public boolean isDomainCharacter(char character) {
-        return character == this.character;
-    }
+        @Override
+        public boolean isDomainCharacter(char character) {
+            return character == this.character;
+        }
 
-    @Override
-    public KeyStrokeCount keyStrokeOfCharacter(SpyterCharacter character) {
-        throw new UnsupportedOperationException("Not implemented");
+        @Override
+        public KeyStrokeCount keyStrokeOfCharacter(SpyterCharacter character) {
+            throw new UnsupportedOperationException("Not implemented");
+        }
     }
 }
