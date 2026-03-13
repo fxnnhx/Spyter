b0954d415dce9977804278751336816cd0d45adf
key.collector <key.collector@web.de>
2026-03-13 12:31:47 +0100

    define Action switch in exercise main loop

--- 2-Spyter-application/src/main/java/de/dhbw/ase/RunExercise.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/RunExercise.java
@@ -1,8 +1,8 @@
 package de.dhbw.ase;
 
-import de.dhbw.ase.entities.Exercise;
 import de.dhbw.ase.entities.ExerciseEvaluator;
 import de.dhbw.ase.entities.TextProgress;
+import de.dhbw.ase.valueObjects.SpyterCharacter;
 
 import java.util.Optional;
 
@@ -15,10 +15,24 @@ public class RunExercise {
 
     public Optional<ExerciseEvaluator> run(UIHandle ui) {
         while (!exercise.isFinished()) {
-
+            switch (ui.getNextAction()){
+                case Action.Exit () -> handleExit(ui);
+                case Action.RemovedChar () -> handleRemovedChar(ui);
+                case Action.TypedChar typedChar -> handleTypedChar(ui, typedChar.getCharacter());
+            }
         }
 
         return Optional.empty();
         //return this.exercise.toEvaluator();
+    }
+
+    private void handleTypedChar(UIHandle ui, SpyterCharacter character) {
+    }
+
+    private void handleRemovedChar(UIHandle ui) {
+    }
+
+    private void handleExit(UIHandle ui) {
+
     }
 }
--- 2-Spyter-application/src/main/java/de/dhbw/ase/UIHandle.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/UIHandle.java
@@ -1,4 +1,5 @@
 package de.dhbw.ase;
 
 public interface UIHandle {
+    Action getNextAction();
 }
