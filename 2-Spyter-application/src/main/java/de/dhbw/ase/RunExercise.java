4d27fb2b07dec70dbac5a10f01d937899e019bb6
key.collector <key.collector@web.de>
2026-03-13 12:07:35 +0100

    define exercise main loop

--- 2-Spyter-application/src/main/java/de/dhbw/ase/RunExercise.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/RunExercise.java
@@ -1,4 +1,24 @@
 package de.dhbw.ase;
 
+import de.dhbw.ase.entities.Exercise;
+import de.dhbw.ase.entities.ExerciseEvaluator;
+import de.dhbw.ase.entities.TextProgress;
+
+import java.util.Optional;
+
 public class RunExercise {
+    TextProgress exercise;
+
+    public RunExercise(TextProgress exercise) {
+        this.exercise = exercise;
+    }
+
+    public Optional<ExerciseEvaluator> run(UIHandle ui) {
+        while (!exercise.isFinished()) {
+
+        }
+
+        return Optional.empty();
+        //return this.exercise.toEvaluator();
+    }
 }
