52ddf065afbf6e2ff1535173639f30c247205f56
key.collector <key.collector@web.de>
2026-03-13 12:11:47 +0100

    implement action sealed interface

--- 2-Spyter-application/src/main/java/de/dhbw/ase/Action.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/Action.java
@@ -1,4 +1,28 @@
 package de.dhbw.ase;
 
-public interface Action {
+import de.dhbw.ase.valueObjects.SpyterCharacter;
+
+public sealed interface Action {
+    public SpyterCharacter getCharacter();
+
+    record Exit() implements Action {
+        @Override
+        public SpyterCharacter getCharacter() {
+            throw new UnsupportedOperationException("Not implemented");
+        }
+    }
+
+    record TypedChar(SpyterCharacter c) implements Action {
+        @Override
+        public SpyterCharacter getCharacter() {
+            return c;
+        }
+    }
+
+    record RemovedChar() implements Action {
+        @Override
+        public SpyterCharacter getCharacter() {
+            throw new UnsupportedOperationException("Not implemented");
+        }
+    }
 }
