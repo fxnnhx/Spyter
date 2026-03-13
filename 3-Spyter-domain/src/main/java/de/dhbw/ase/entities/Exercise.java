84cd3547d4570c8352fbadd10623ede24f307c4a
key.collector <key.collector@web.de>
2026-03-13 12:51:33 +0100

    define methods

--- 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/Exercise.java
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/entities/Exercise.java
@@ -1,4 +1,24 @@
 package de.dhbw.ase.entities;
 
+import de.dhbw.ase.valueObjects.AdvanceType;
+import de.dhbw.ase.valueObjects.SpyterCharacter;
+
 public class Exercise {
+
+    public boolean isFinished() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public AdvanceType takeCharacter(SpyterCharacter character) {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public void deleteLastCharacter() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
+    public ExerciseEvaluator toEvaluator() {
+        throw new UnsupportedOperationException("Not implemented yet.");
+    }
+
 }
