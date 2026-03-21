7982efefbcc592b888acf3eb4692c63ce33eae63
key.collector <key.collector@web.de>
2026-03-21 09:31:19 +0100

    implement configuration method

--- 2-Spyter-application/src/main/java/de/dhbw/ase/ConfigureExercise.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/ConfigureExercise.java
@@ -1,11 +1,18 @@
 package de.dhbw.ase;
 
+import de.dhbw.ase.entities.Corrector;
 import de.dhbw.ase.entities.Exercise;
+import de.dhbw.ase.entities.TextProgress;
+import de.dhbw.ase.valueObjects.SpyterText;
+import de.dhbw.ase.valueObjects.TextGenerator;
 
 public class ConfigureExercise {
 
     public Exercise configure(ConfigUIHandle uiHandle){
-
+        SpyterText text = uiHandle.getBaseText();
+        TextGenerator generator = uiHandle.getGenerator();
+        Corrector corrector = uiHandle.getCorrector();
+        return new Exercise(corrector, new TextProgress(generator.generate(text)));
     }
 
 }
