88fbef3d107c3a57af0c7d1e8f5755f1991df48d
key.collector <key.collector@web.de>
2026-03-21 09:30:46 +0100

    define ConfigUI

--- 2-Spyter-application/src/main/java/de/dhbw/ase/ConfigUIHandle.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/ConfigUIHandle.java
@@ -1,4 +1,13 @@
 package de.dhbw.ase;
 
+import de.dhbw.ase.entities.Corrector;
+import de.dhbw.ase.valueObjects.SpyterText;
+import de.dhbw.ase.valueObjects.TextGenerator;
+
 public interface ConfigUIHandle {
+    SpyterText getBaseText();
+
+    TextGenerator getGenerator();
+
+    Corrector getCorrector();
 }
