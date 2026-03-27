6b05456642ae5fe1aebfd751eeebe07053b25529
key.collector <key.collector@web.de>
2026-03-27 12:41:25 +0100

    implement Single Instance of Main Workflow

---
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/MainWorkflow.java
@@ -0,0 +1,26 @@
+package de.dhbw.ase;
+
+import de.dhbw.ase.entities.Exercise;
+import de.dhbw.ase.entities.ExerciseEvaluator;
+
+import java.util.Optional;
+
+public class MainWorkflow {
+    ConfigUIHandle configUI;
+    RunningExerciseUI runningExerciseUI;
+    ResultUI resultUI;
+
+    public MainWorkflow(ConfigUIHandle configUI, RunningExerciseUI runningExerciseUI, ResultUI resultUI) {
+        this.configUI = configUI;
+        this.runningExerciseUI = runningExerciseUI;
+        this.resultUI = resultUI;
+    }
+
+    public void runSingleInstance() {
+        Exercise currentExercise = new ConfigureExercise().configure(configUI);
+        Optional<ExerciseEvaluator> currentEvaluator = new RunExercise(currentExercise).run(runningExerciseUI);
+        currentEvaluator.ifPresentOrElse(
+                evaluator -> resultUI.displayResult(new GenerateExerciseResults().getResult(evaluator)),
+                resultUI::displayInterrupt);
+    }
+}
