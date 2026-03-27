b9a273b8db9f4deb92b27d3b870294d28e17d993
key.collector <key.collector@web.de>
2026-03-27 12:41:03 +0100

    define ResultUI

---
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/ResultUI.java
@@ -0,0 +1,8 @@
+package de.dhbw.ase;
+
+import de.dhbw.ase.valueObjects.ExerciseResult;
+
+public interface ResultUI {
+    public void displayResult(ExerciseResult result);
+    public void displayInterrupt();
+}
