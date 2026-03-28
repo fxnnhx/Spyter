8e69525654c51d7a28e7aacfaab8e4abcb29e31c
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-03-28 17:58:52 +0100

    remove not needed interface fn

--- 2-Spyter-application/src/main/java/de/dhbw/ase/Action.java
+++ 2-Spyter-application/src/main/java/de/dhbw/ase/Action.java
@@ -3,26 +3,9 @@ package de.dhbw.ase;
 import de.dhbw.ase.valueObjects.SpyterCharacter;
 
 public sealed interface Action {
-    public SpyterCharacter getCharacter();
+    record Exit() implements Action {}
 
-    record Exit() implements Action {
-        @Override
-        public SpyterCharacter getCharacter() {
-            throw new UnsupportedOperationException("Not implemented");
-        }
-    }
+    record TypedChar(SpyterCharacter c) implements Action {}
 
-    record TypedChar(SpyterCharacter c) implements Action {
-        @Override
-        public SpyterCharacter getCharacter() {
-            return c;
-        }
-    }
-
-    record RemovedChar() implements Action {
-        @Override
-        public SpyterCharacter getCharacter() {
-            throw new UnsupportedOperationException("Not implemented");
-        }
-    }
+    record RemovedChar() implements Action {}
 }
