58e67cf7c56f6572c4d582e66494853a5dfd4676
key.collector <key.collector@web.de>
2026-03-31 09:20:46 +0200

    define constructor and members

--- 0-Spyter-plugin-database/src/main/java/de/dhbw/ase/SimpleTerminalIO.java
+++ 0-Spyter-plugin-database/src/main/java/de/dhbw/ase/SimpleTerminalIO.java
@@ -2,8 +2,24 @@ package de.dhbw.ase;
 
 import de.dhbw.ase.tui.TerminalIO;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.util.Scanner;
+
 public class SimpleTerminalIO implements TerminalIO {
 
+    PrintStream output;
+    Scanner input;
+
+    public SimpleTerminalIO(OutputStream output, InputStream input) {
+        this.output = new PrintStream(output);
+        this.input = new Scanner(input);
+    }
+
+    public SimpleTerminalIO() {
+        new SimpleTerminalIO(System.out, System.in);
+    }
 
     @Override
     public void writeLine(String text) {
