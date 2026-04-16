d8ac3e3afaa3aaa11accc4c6fb3526bfbe39f784
Finn Hennemann <hennemann.finn.a23@student.dhbw-karlsruhe.de>
2026-04-16 13:46:03 +0200

    add old Main functions to be working again

---
+++ 1-Spyter-adapters/src/main/java/de/dhbw/ase/tui/Main.java
@@ -0,0 +1,7 @@
+package de.dhbw.ase.tui;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ 3-Spyter-domain/src/main/java/de/dhbw/ase/Main.java
@@ -0,0 +1,7 @@
+package de.dhbw.ase;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
